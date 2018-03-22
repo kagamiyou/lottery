352a0797c3d10b865e83dda342e035f4292c8f39
DESKTOP-7FMJO6C\bevis <bevistsou@gmail.com>
2018-03-22 15:53:23 +0800

    亂數

---
+++ lottery/lottery/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            rand.Next(0, 100);
+        }
+    }
+}
