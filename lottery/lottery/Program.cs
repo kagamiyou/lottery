df2b28ca56b2d51ac5a843257163a11d4ca31fae
DESKTOP-7FMJO6C\bevis <bevistsou@gmail.com>
2018-03-22 16:06:51 +0800

    雛形

--- lottery/lottery/Program.cs
+++ lottery/lottery/Program.cs
@@ -10,8 +10,22 @@ namespace lottery
     {
         static void Main(string[] args)
         {
+            //產生六個亂數
             Random rand = new Random();
-            rand.Next(0, 100);
+            int r1 = rand.Next(1, 43);
+            int r2 = rand.Next(1, 43);
+            int r3 = rand.Next(1, 43);
+            int r4 = rand.Next(1, 43);
+            int r5 = rand.Next(1, 43);
+            int r6 = rand.Next(1, 43);
+
+            
+
+            //印出
+            Console.WriteLine("{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+
+            //防止視窗自動關閉
+            Console.ReadLine();
         }
     }
 }
